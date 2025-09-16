e4f07a63548f1eeef4320e305ea7a442785b86dc
French Bear <frenchbear38@outlook.com>
2025-09-16 22:27:19 +0200

    save

--- Net9/301-319/305 VB RotorRouter/form RotorRouter.vb
+++ Net9/301-319/305 VB RotorRouter/form RotorRouter.vb
@@ -20,38 +20,35 @@ Public Class frmRotorRouter
     Private ReadOnly col2 As Color = Color.White
     Private ReadOnly col3 As Color = Color.Yellow
 
-    'ReadOnly col0 As Color = Color.LightBlue
-    'ReadOnly col1 As Color = Color.Blue
-    'ReadOnly col2 As Color = Color.LightGreen
-    'ReadOnly col3 As Color = Color.Green
+    'Private ReadOnly col0 As Color = Color.LightBlue
+    'Private ReadOnly col1 As Color = Color.Blue
+    'Private ReadOnly col2 As Color = Color.LightGreen
+    'Private ReadOnly col3 As Color = Color.Green
 
     Private ReadOnly colBackground As Color = Color.PaleGoldenrod
 
-    Private ReadOnly r As Integer = 100
-    Private ReadOnly n = 15000
-    Private ReadOnly nRefresh = 50      ' Refres every nRefresh passes
-
-    Private ReadOnly picScale As Integer = 2
+    Private ReadOnly r As Integer = 100         ' Circle radius = 1/2 size of square picture
+    Private ReadOnly n = 15000                  ' Number of iterations
+    Private ReadOnly nRefresh = 50              ' Refresh image every nRefresh iterations
+    Private ReadOnly picScale As Integer = 2    ' Scaling factor for display
 
     Private Sub btnStart_Click(sender As Object, e As EventArgs) Handles btnStart.Click
         btnStart.Enabled = False
 
+        ' PicBitmap is the actual bitmap surface the algorithm is running on
         Dim picBitmap As New Bitmap((2 * r) + 1, (2 * r) + 1, PixelFormat.Format24bppRgb)
+        ' Pic rescaled is a copy of picBitmap taken every nRefresh iterations, rescaled of a factor picScale, and displayed
         Dim picRescaled As New Bitmap(picScale * ((2 * r) + 1), picScale * ((2 * r) + 1), PixelFormat.Format24bppRgb)
+
         Graphics.FromImage(picBitmap).Clear(colBackground)
         pic.Width = picScale * ((2 * r) + 1)
         pic.Height = picScale * ((2 * r) + 1)
         Panel1.Refresh()
 
-        ' Create rectangle for displaying image.
-        Dim destRect As New Rectangle(0, 0, picScale * ((2 * r) + 1), picScale * ((2 * r) + 1))
-
         For i As Integer = 0 To n
             If i Mod nRefresh = 0 Then
                 txtIterations.Text = i.ToString
                 txtIterations.Refresh()
-                'pic.BackgroundImage = picBitmap.GetThumbnailImage(picScale * (2 * r + 1), picScale * (2 * r + 1), Nothing, Nothing)
-                'pic.Refresh()
 
                 Dim g As Graphics = Graphics.FromImage(picRescaled)
                 g.SmoothingMode = SmoothingMode.None
@@ -62,15 +59,9 @@ Public Class frmRotorRouter
                 Application.DoEvents()
             End If
 
-            Dim x, y As Integer
-            x = r
-            y = r
+            ' Start at the center for each iteration
+            Dim x = r, y = r
             Do
-                'Dim c As Color = picBitmap.GetPixel(x, y)
-                'If c.ToArgb = colBackground.ToArgb Then
-                'picBitmap.SetPixel(x, y, col0)
-                'Exit Do
-                'End If
                 Select Case picBitmap.GetPixel(x, y).ToArgb
                     Case colBackground.ToArgb
                         picBitmap.SetPixel(x, y, col0)
@@ -96,7 +87,7 @@ Public Class frmRotorRouter
         btnStart.Enabled = True
     End Sub
 
-    ' Because of DoEvents
+    ' DoEvents allows the app to be closed before it terminates
     Private Sub frmRotorRouter_FormClosed(sender As Object, e As FormClosedEventArgs) Handles Me.FormClosed
         End
     End Sub
